9e7727e4d3ee2f8ca0ca2018ffd47f56d3aabfb8
Sacha Laville <sacha_laville@outlook.fr>
2025-04-23 23:14:18 +0200

    Update Home Page

--- src/js/script.js
+++ src/js/script.js
@@ -6,11 +6,11 @@ const themeSwitch = document.querySelector('#themeSwitch');
 moonSwitchBtn.addEventListener('click', (e) => {
     moonSwitchBtn.style.display = 'none';
     sunSwitchBtn.style.display = 'block';
-    themeSwitch.href = './assets/css/lightStyle.css'
+    themeSwitch.href = './src/css/lightStyle.css'
 })
 
 sunSwitchBtn.addEventListener('click', (e) => {
     moonSwitchBtn.style.display = 'block';
     sunSwitchBtn.style.display = 'none';
-    themeSwitch.href = './assets/css/darkStyle.css'
+    themeSwitch.href = './src/css/style.css'
 })
