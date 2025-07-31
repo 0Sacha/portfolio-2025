67e4386680e63341f1140db0751fd14bd0a04faa
Sacha Laville <sacha_laville@outlook.fr>
2025-07-31 16:27:33 +0200

    Add light/dark mode toggle with sun icon
    Introduced a sun.svg icon and updated index.html to include both moon and sun icons for toggling dark and light modes. Added CSS variables for light and dark navbar colors, refactored styles to use these variables, and updated main and nav styling. Enhanced script.js to toggle visibility between sun and moon icons on click, preparing for light/dark mode functionality.

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,11 @@ const closeMenu = document.querySelector('#close-menu');
 const navMenu = document.querySelector('.menu-popup');
 
 const html = document.querySelector('html');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
 
 const moon = document.querySelector('#moon');
+const sun = document.querySelector('#sun');
 
 
 menuHamburger.addEventListener('click', (e) => {
@@ -20,6 +23,9 @@ menuHamburger.addEventListener('click', (e) => {
     menuHamburger.style.display = 'none';
     moon.style.display = 'none';
     closeMenu.style.display = 'block';
+
+    // body.style.background = "#F8F8F8FF"
+
 })
 
 closeMenu.addEventListener('click', (e) => {
@@ -27,6 +33,16 @@ closeMenu.addEventListener('click', (e) => {
     menuHamburger.style.display = 'block';
     moon.style.display = 'block';
     closeMenu.style.display = 'none';
+})
+
+moon.addEventListener('click', (e) => {
+    moon.style.display = 'none';
+    sun.style.display = 'block';
+})
+
+sun.addEventListener('click', (e) => {
+    sun.style.display = 'none';
+    moon.style.display = 'block';
 })
 
 
