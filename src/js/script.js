872ddc77242facb7f24d2a2b2afb2c86cdb9f2e8
Sacha Laville <sacha_laville@outlook.fr>
2025-09-10 15:42:35 +0200

    +add: Links of my social network (github, linkedin, mail) under my image of profile

--- src/js/script.js
+++ src/js/script.js
@@ -39,24 +39,42 @@ const moon = document.querySelector('#moon');
 const sun = document.querySelector('#sun');
 const home = document.querySelector('#home');
 
+const linkedin = document.querySelector('#linkedin');
+const github = document.querySelector('#github');
+const mailto = document.querySelector('#mailto');
+
 // code //
 
 moon.addEventListener('click', (e) => {
+
+    // .display
+
     moon.style.display = 'none';
     sun.style.display = 'block';
+
+    // .background
 
     nav_menu.style.background = 'var(--dark-bg-navbar-color)'
     main.style.background = 'var(--bg-color-dark)'
     html.style.background = 'var(--dark-html-bg-color)';
+    scrollToUp.style.backgroundColor = 'var(--dark-bg-navbar-color)';
+
+    // .color
 
     home.style.color = 'var(--dark-text-navbar-color)';
     nav.style.color = 'var(--dark-text-navbar-color)';
 
-    scrollToUp.style.backgroundColor = 'var(--dark-bg-navbar-color)';
+    // .src
 
     menu.src = 'src/img/menu-light.svg';
     close_menu.src = './src/img/close-menu-light.svg';
     scrollToUp.src = './src/img/up-arrow-light.svg'
+    // links
+    linkedin.src = './src/img/icons/linkedin-light-icon.svg'
+    github.src = './src/img/icons/github-light-icon.svg'
+    mailto.src = './src/img/icons/mailto-light-icon.svg'
+
+    // forEach
 
     nav_a.forEach(a => {
         a.style.color = 'var(--dark-text-navbar-color)';
@@ -68,21 +86,35 @@ moon.addEventListener('click', (e) => {
 })
 
 sun.addEventListener('click', (e) => {
+
+    // .display
+
     sun.style.display = 'none';
     moon.style.display = 'block';
+
+    // .background
 
     nav_menu.style.background = 'var(--light-bg-navbar-color)'
     main.style.background = 'var(--bg-color-light)'
     html.style.background = 'var(--light-html-bg-color)';
+    scrollToUp.style.backgroundColor = 'var(--light-bg-navbar-color)';
+
+    // .color
 
     home.style.color = 'var(--light-text-navbar-color)';
     nav.style.color = 'var(--light-text-navbar-color)';
 
-    scrollToUp.style.backgroundColor = 'var(--light-bg-navbar-color)';
+    // .src
 
     menu.src = 'src/img/menu-dark.svg';
     close_menu.src = './src/img/close-menu-dark.svg';
     scrollToUp.src = './src/img/up-arrow-dark.svg'
+    // links
+    linkedin.src = './src/img/icons/linkedin-dark-icon.svg'
+    github.src = './src/img/icons/github-dark-icon.svg'
+    mailto.src = './src/img/icons/mailto-dark-icon.svg'
+
+    // forEach
 
     nav_a.forEach(a => {
         a.style.color = 'var(--light-text-navbar-color)';
@@ -141,8 +173,11 @@ const scrollToUp = document.querySelector('#scrollToUp');
 window.addEventListener('scroll', (e) => {
     if (window.scrollY > 1) {
         scrollToUp.style.display = 'block';
+        scrollToUp.style.transition = 'all 0.3s ease';
+
     } else {
         scrollToUp.style.display = 'none';
+        scrollToUp.style.transition = 'all 0.3s ease';
     }
 })
 
