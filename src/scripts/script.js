bb10ec8f69b2685be6a5f376c0dfe4e2a85b4ef4
Sacha Laville <sacha_laville@outlook.fr>
2025-11-06 15:37:06 +0100

    +update: finalisation de la partie des projets marquant et ajout de la partie projet 'normaux'

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,3 +15,15 @@ closeBurger.addEventListener('click', () => {
     closeBurger.style.display = 'none';
     openElement.style.display = 'none';
 })
+
+// navbar sizing to scroll
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 1) {
+        nav.style.scale = '1.03';
+    } else {
+        nav.style.scale = '1.0';
+    }
+})
