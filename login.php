fdbf49b23662066cc8e0c0c66c4591caed24b6d8
Sacha Laville <sacha_laville@outlook.fr>
2025-11-10 23:16:40 +0100

    +add: début de l'implementation de la section expérience / A propos

---
+++ login.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no">
+
+    <title>Porfolio 2025 - Sacha Laville</title>
+
+    <!--    balises meta réseaux sociaux-->
+    <meta content="Porfolio 2025 - Sacha Laville"
+          property="og:title" name="og:title">
+    <meta content="Étudiant en deuxième année du BTS SIO SLAM à Promeo Compiègne.
+    Ce portfolio servira de support pour un oral qui contribuera à l'obtention de mon diplôme.
+    Celui-ci sert entre autres à montrer mes compétences en matière de programmation."
+          property="og:description" name="og:description">
+    <meta content="https://sachalaville.fr/src/img/meta_banner.png"
+          property="og:image" name="og:image">
+    <meta content="1200"
+          property="og:image:width" name="og:image:width">
+    <meta content="630"
+          property="og:image:height" name="og:image:height">
+    <meta content="https://sachalaville.fr/"
+          property="og:url" name="og:url">
+    <meta content="Portfolio 2025 - Sacha Laville"
+          property="og:site_name" name="og:site_name">
+    <meta content="website"
+          property="og:type" name="og:type">
+    <meta content="fr_FR"
+          property="og:locale" name="og:locale">
+
+    <link rel="icon" href="src/images/LogoPortfolio.png" type="image/x-icon">
+    <link rel="stylesheet" href="./src/css/style.css">
+    <script src="src/scripts/script.js" defer></script>
+</head>
+<body>
+<div id="loginCard">
+    <div id="loginLogo">
+
+    </div>
+    <h1>Dashboard Admin</h1>
+    <p>Connectez-vous pour accéder au CMS</p>
+    <form action="" method="get">
+        <label for="Email">
+            <input type="email" placeholder="admin@admin.com">
+        </label>
+        <label for="Mots de passe">
+            <input type="password" placeholder="••••••••">
+        </label>
+        <button formmethod="post">Se connecter</button>
+    </form>
+</div>
+</body>
+</html>
