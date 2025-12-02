e311e98462a0a1618b3c5243f5d5582eeb720acc
Sacha Laville <sacha_laville@outlook.fr>
2025-12-02 16:59:57 +0100

    +update: adaptation de la partie selection pour la version mobile

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,22 +1,4 @@
-// burger menu
-
-const openBurger = document.querySelector('#openBurger');
-const closeBurger = document.querySelector('#closeBurger');
-const openElement = document.querySelector('.burger');
-
-openBurger.addEventListener('click', () => {
-    openBurger.style.display = 'none';
-    closeBurger.style.display = 'block';
-    openElement.style.display = 'flex';
-})
-
-closeBurger.addEventListener('click', () => {
-    openBurger.style.display = 'block';
-    closeBurger.style.display = 'none';
-    openElement.style.display = 'none';
-})
-
-// navbar sizing to scroll
+// navbar re-sizing to scroll
 
 const nav = document.querySelector('nav');
 
