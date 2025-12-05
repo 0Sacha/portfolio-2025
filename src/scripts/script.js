f32f6073885bc31bf1859a6dc1545f9cca365280
Sacha Laville <sacha_laville@outlook.fr>
2025-12-05 12:00:01 +0100

    +update: début de l'adaptabilité de la section About Me sur mobile

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -12,52 +12,6 @@ window.addEventListener('scroll', () => {
 
 //
 
-const selectBtn1 = document.querySelector('#selectBtn1')
-const selectBtn2 = document.querySelector('#selectBtn2')
-const selectBtn3 = document.querySelector('#selectBtn3')
-
-const circle1 = document.querySelector('.circle1')
-const circle2 = document.querySelector('.circle2')
-const circle3 = document.querySelector('.circle3')
-
-
-selectBtn1.addEventListener('click', (e) => {
-    circle1.style.width = '64px'
-    circle1.style.backgroundColor = '#51A2FF'
-    circle2.style.width = '16px'
-    circle2.style.backgroundColor = 'rgba(255,255,255,0.2)'
-    circle3.style.width = '16px'
-    circle3.style.backgroundColor = 'rgba(255,255,255,0.2)'
-})
-
-if (window.location.href === 'http://localhost:63342/portfolio-2025/index.html?_ijt=caqffodlu47qagr9b29h3ncfvt&_ij_reload=RELOAD_ON_SAVE#selectionCard01') {
-    circle1.style.width = '64px'
-    circle1.style.backgroundColor = '#51A2FF'
-} else {
-    circle1.style.width = '16px'
-    circle1.style.backgroundColor = 'rgba(255,255,255,0.2)'
-}
-
-selectBtn2.addEventListener('click', (e) => {
-    circle1.style.width = '16px'
-    circle1.style.backgroundColor = 'rgba(255,255,255,0.2)'
-    circle2.style.width = '64px'
-    circle2.style.backgroundColor = '#51A2FF'
-    circle3.style.width = '16px'
-    circle3.style.backgroundColor = 'rgba(255,255,255,0.2)'
-})
-
-selectBtn3.addEventListener('click', (e) => {
-    circle1.style.width = '16px'
-    circle1.style.backgroundColor = 'rgba(255,255,255,0.2)'
-    circle2.style.width = '16px'
-    circle2.style.backgroundColor = 'rgba(255,255,255,0.2)'
-    circle3.style.width = '64px'
-    circle3.style.backgroundColor = '#51A2FF'
-})
-
-//
-
 const change = document.querySelector('#hamburgerBtn');
 const menu = document.querySelector('#mobile');
 change.addEventListener('click', (e) => {
