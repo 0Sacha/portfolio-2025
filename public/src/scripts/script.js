224e22fd6b50771e47374235db119028492ae033
Sacha Laville <sacha_laville@outlook.fr>
2026-02-16 00:25:51 +0100

    update: refont de la navbar

--- public/src/scripts/script.js
+++ public/src/scripts/script.js
@@ -261,12 +261,15 @@ async function deleteMessage(messageId) {
 // navbar re-sizing to scroll
 
 const nav = document.querySelector('#navbar');
+const promeo = document.querySelector('#promeo');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 1) {
         nav.style.scale = '1.03';
+        promeo.style.scale = '1.03';
     } else {
         nav.style.scale = '1.0';
+        promeo.style.scale = '1.0';
     }
 })
 
