aa183759e5e9616f8b3896f2439b7df4a2947398
Sacha Laville <sacha_laville@outlook.fr>
2026-02-25 00:00:16 +0100

    big-update: J'ai remplacé l'intégralité des icônes brutes par des icônes FontAwesome afin d'alléger le chargement du site, ce qui a entraîné la suppression des anciennes icônes dans les dossiers du site).

--- public/src/scripts/script.js
+++ public/src/scripts/script.js
@@ -82,12 +82,7 @@ function chargerMessages() {
                     messagesSection.innerHTML += `
                     <div class="messages-card" data-id="${msg.id}">
                         <div class="messages-titre">
-                            <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M15.8332 17.5V15.8333C15.8332 14.9493 15.482 14.1014 14.8569 13.4763C14.2317 12.8512 13.3839 12.5 12.4998 12.5H7.49984C6.61578 12.5 5.76794 12.8512 5.14281 13.4763C4.51769 14.1014 4.1665 14.9493 4.1665 15.8333V17.5"
-                                 stroke="white" stroke-opacity="1" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M9.99984 9.16667C11.8408 9.16667 13.3332 7.67428 13.3332 5.83333C13.3332 3.99238 11.8408 2.5 9.99984 2.5C8.15889 2.5 6.6665 3.99238 6.6665 5.83333C6.6665 7.67428 8.15889 9.16667 9.99984 9.16667Z"
-                                 stroke="white" stroke-opacity="1" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
+                            <i class="fa-solid fa-user-astronaut" style="color : white"></i>
                             <p class="messages-nom">${msg.nom}</p>
                             ${msg.is_new === 1 ? '<span class="messages-new">New</span>' : ''}
                         </div>
@@ -155,59 +150,26 @@ async function readMessage(messageId) {
                 <div class="messages-content">
                 <p class="messages-sujet">${msg.sujet}</p>
                 <div class="messages-nom-content">
-                    <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15.8332 17.5V15.8333C15.8332 14.9493 15.482 14.1014 14.8569 13.4763C14.2317 12.8512 13.3839 12.5 12.4998 12.5H7.49984C6.61578 12.5 5.76794 12.8512 5.14281 13.4763C4.51769 14.1014 4.1665 14.9493 4.1665 15.8333V17.5"
-                              stroke="white" stroke-opacity="1" stroke-width="1.66667" stroke-linecap="round"
-                              stroke-linejoin="round"/>
-                        <path d="M9.99984 9.16667C11.8408 9.16667 13.3332 7.67428 13.3332 5.83333C13.3332 3.99238 11.8408 2.5 9.99984 2.5C8.15889 2.5 6.6665 3.99238 6.6665 5.83333C6.6665 7.67428 8.15889 9.16667 9.99984 9.16667Z"
-                              stroke="white" stroke-opacity="1" stroke-width="1.66667" stroke-linecap="round"
-                              stroke-linejoin="round"/>
-                    </svg>
+                    <i class="fa-regular fa-user"></i>
                     <p class="messages-nom-p">${msg.nom}</p>
                 </div>
                 <div class="messages-email-content">
-                    <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18.3337 5.83301L10.8412 10.6055C10.5869 10.7532 10.2981 10.831 10.0041 10.831C9.71004 10.831 9.42125 10.7532 9.16699 10.6055L1.66699 5.83301"
-                              stroke="#FFFFFF" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M16.667 3.33301H3.33366C2.41318 3.33301 1.66699 4.0792 1.66699 4.99967V14.9997C1.66699 15.9201 2.41318 16.6663 3.33366 16.6663H16.667C17.5875 16.6663 18.3337 15.9201 18.3337 14.9997V4.99967C18.3337 4.0792 17.5875 3.33301 16.667 3.33301Z"
-                              stroke="#FFFFFF" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <i class="fa-regular fa-envelope"></i>
                     <p class="messages-email">${msg.email}</p>
                 </div>
                 <div class="messages-time-content">
-                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 6V12L16 14" stroke="#00D3F2" stroke-width="2" stroke-linecap="round"
-                              stroke-linejoin="round"/>
-                        <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-                              stroke="#00D3F2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <i class="fa-regular fa-clock"></i>
                     <p class="messages-temps">le ${new Date(msg.data_envoi).toLocaleDateString()}</p>
                 </div>
                 <div class="messages-contenu"><p class="messages-contenu-p">${msg.contenu.substring(0, 40)}</p></div>
             </div>
             <div class="messages-actions">
                 <div class="messages-reponse">
-                    <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18.3337 5.83301L10.8412 10.6055C10.5869 10.7532 10.2981 10.831 10.0041 10.831C9.71004 10.831 9.42125 10.7532 9.16699 10.6055L1.66699 5.83301"
-                              stroke="#FFFFFF" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M16.667 3.33301H3.33366C2.41318 3.33301 1.66699 4.0792 1.66699 4.99967V14.9997C1.66699 15.9201 2.41318 16.6663 3.33366 16.6663H16.667C17.5875 16.6663 18.3337 15.9201 18.3337 14.9997V4.99967C18.3337 4.0792 17.5875 3.33301 16.667 3.33301Z"
-                              stroke="#FFFFFF" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <i class="fa-regular fa-clock" style="color:white;"></i>
                     <p class="messages-reponse-p">Répondre</p>
                 </div>
                 <div class="messages-supprimer">
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M2 4H14" stroke="white" stroke-width="1.33333" stroke-linecap="round"
-                              stroke-linejoin="round"/>
-                        <path d="M12.6668 4V13.3333C12.6668 14 12.0002 14.6667 11.3335 14.6667H4.66683C4.00016 14.6667 3.3335 14 3.3335 13.3333V4"
-                              stroke="white" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M5.3335 4.00004V2.66671C5.3335 2.00004 6.00016 1.33337 6.66683 1.33337H9.3335C10.0002 1.33337 10.6668 2.00004 10.6668 2.66671V4.00004"
-                              stroke="white" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M6.6665 7.33337V11.3334" stroke="white" stroke-width="1.33333" stroke-linecap="round"
-                              stroke-linejoin="round"/>
-                        <path d="M9.3335 7.33337V11.3334" stroke="white" stroke-width="1.33333" stroke-linecap="round"
-                              stroke-linejoin="round"/>
-                    </svg>
+                    <i class="fa-regular fa-trash-can" style="color:white;"></i>
                 </div>
             </div>
                 `;
@@ -248,7 +210,7 @@ async function deleteMessage(messageId) {
             }
 
             detailsContainer.innerHTML = `
-            <img class="messages-icon" src="./src/images/icons/dashboard/message.svg" alt="">
+            <i class="fa-regular fa-message"></i>
             <p class="messages-notfound">Sélectionnez un message pour voir les détails</p>
             `;
 
